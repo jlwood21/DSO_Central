6c9f720ee38c7726e9526beb71f36fb248db5c7d
jlwood21 <jacklw21@gmail.com>
2023-08-28 14:59:50 +0100

    new files added

--- 3D-pipeline.js
+++ 3D-pipeline.js
@@ -1,33 +1,35 @@
-// Initialize scene, camera, and renderer
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(
-  75,
-  window.innerWidth / window.innerHeight,
-  0.1,
-  1000,
-);
-const renderer = new THREE.WebGLRenderer();
-
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.getElementById("3D-pipeline").appendChild(renderer.domElement);
-
-// Create a cube
-const geometry = new THREE.BoxGeometry();
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-const cube = new THREE.Mesh(geometry, material);
-
-scene.add(cube);
-
-camera.position.z = 5;
-
-// Animation
-const animate = function () {
-  requestAnimationFrame(animate);
-
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
-
-  renderer.render(scene, camera);
-};
-
-animate();
+document.addEventListener("DOMContentLoaded", function() {
+    // Initialize the scene, camera, and renderer
+    const scene = new THREE.Scene();
+    const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+    const renderer = new THREE.WebGLRenderer();
+  
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.getElementById('three-container').appendChild(renderer.domElement);
+  
+    // Create the geometry and material for the pipeline
+    const geometry = new THREE.TorusGeometry(10, 3, 16, 100);
+    const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+    const torus = new THREE.Mesh(geometry, material);
+  
+    // Add the pipeline to the scene
+    scene.add(torus);
+  
+    // Position the camera
+    camera.position.z = 50;
+  
+    // Animation function
+    function animate() {
+      requestAnimationFrame(animate);
+  
+      // Rotate the pipeline
+      torus.rotation.x += 0.01;
+      torus.rotation.y += 0.01;
+  
+      renderer.render(scene, camera);
+    }
+  
+    // Start the animation
+    animate();
+  });
+  
