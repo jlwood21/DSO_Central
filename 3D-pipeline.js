ef86c045534cb60337a33fb0d36ae4df8dd90e3a
jlwood21 <jacklw21@gmail.com>
2023-09-01 20:45:47 +0100

    new files added

--- 3D-pipeline.js
+++ 3D-pipeline.js
@@ -1,22 +1,27 @@
 // Initialize the scene, camera, and renderer
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-const renderer = new THREE.WebGLRenderer({ alpha: true });  // Added alpha: true for transparency
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000,
+);
+const renderer = new THREE.WebGLRenderer({ alpha: true });
 
 // Get the container element and set renderer size
-const container = document.getElementById('threeD-pipeline-container');
+const container = document.getElementById("threeD-pipeline-container");
 renderer.setSize(container.offsetWidth, container.offsetHeight);
 
 // Attach the renderer to the container
 container.appendChild(renderer.domElement);
 
 // Create a geometry and material
-const geometry = new THREE.BoxGeometry();
+const geometry = new THREE.SphereGeometry();
 const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
 
 // Create a mesh and add it to the scene
-const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
+const sphere = new THREE.Mesh(geometry, material);
+scene.add(sphere);
 
 // Position the camera
 camera.position.z = 5;
@@ -25,9 +30,9 @@ camera.position.z = 5;
 const animate = () => {
   requestAnimationFrame(animate);
 
-  // Rotate the cube
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
+  // Rotate the sphere
+  sphere.rotation.x += 0.01;
+  sphere.rotation.y += 0.01;
 
   // Render the scene with the camera
   renderer.render(scene, camera);
